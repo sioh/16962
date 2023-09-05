662a8d41adc2990483d618c750927ff00181030f
Silvio Haehner <sioh@users.noreply.github.com>
2023-09-05 14:21:59 +0200

    Move path creation into promise.

--- aufgabe5neu/webapp/controller/Detail.controller.js
+++ aufgabe5neu/webapp/controller/Detail.controller.js
@@ -13,11 +13,11 @@ sap.ui.define(
 
         _onObjectMatched: function(oEvent) {
           var sCustomerId = oEvent.getParameter("arguments").CustomerId,
-              oModel = this.getView().getModel(),
-              sPath = "/" + oModel.createKey("FlugkundeSet", {Customerid: sCustomerId});
+              oModel = this.getView().getModel();
 
           oModel.metadataLoaded()
             .then(function() {
+              let sPath = "/" + oModel.createKey("FlugkundeSet", {Customerid: sCustomerId});
               this.getView().bindElement(sPath);
             }.bind(this));
         }
